8ff22cfb5507ab554d2b2f3cb33c6f9cad8b9a29
simonkijo <simonkijo@gmail.com>
2023-02-18 15:54:19 +0300

    updating userinterface

--- src/main/java/com/simonkijo/login/loginController.java
+++ src/main/java/com/simonkijo/login/loginController.java
@@ -1,14 +1,43 @@
 package com.simonkijo.login;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class loginController {
     @FXML
-    private Label welcomeText;
+    private Button btnCancel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btnLogin;
+
+    @FXML
+    private Button btnRegister;
+
+    @FXML
+    private TextField passWord;
+
+    @FXML
+    private Label smsStatus;
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    void btnCancelAcction(ActionEvent event) {
+
     }
+
+    @FXML
+    void btnLoginAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegisterAction(ActionEvent event) {
+
+    }
+
 }
