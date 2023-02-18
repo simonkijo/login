2df16577dd51bc699230d63d7c068b6f1c4b9e06
simonkijo <simonkijo@gmail.com>
2023-02-18 13:51:17 +0300

    file name refactoring

--- src/main/java/com/simonkijo/login/loginController.java
+++ src/main/java/com/simonkijo/login/loginController.java
@@ -3,7 +3,7 @@ package com.simonkijo.login;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class loginController {
     @FXML
     private Label welcomeText;
 
