0cf954af2419f4f2921263959a89ae42138352d3
simonkijo <simonkijo@gmail.com>
2023-02-28 19:32:04 +0300

    ADDING UI ELEMENT ID

--- src/main/java/com/simonkijo/login/registerController.java
+++ src/main/java/com/simonkijo/login/registerController.java
@@ -1,5 +1,26 @@
 package com.simonkijo.login;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class registerController {
-    //HELLO KIJO KIJO KIJO KIJO
+    @FXML
+    private Button btnCancel_;
+    @FXML
+    private Button btnRegister_;
+    @FXML
+    private PasswordField cpword;
+    @FXML
+    private TextField fname;
+    @FXML
+    private TextField lname;
+    @FXML
+    private PasswordField pword;
+    @FXML
+    private TextField uname;
+
+
+
 }
