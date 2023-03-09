e359b9d1f65c60ce3a28f5334af88f330e6048f3
simon kijo <simonkijo@gmail.com>
2023-03-09 04:29:59 +0300

    filtering and sorting data on table view

--- src/main/java/com/simonkijo/login/personSearchController.java
+++ src/main/java/com/simonkijo/login/personSearchController.java
@@ -2,6 +2,8 @@ package com.simonkijo.login;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
@@ -36,6 +38,7 @@ public class personSearchController implements Initializable {
     ObservableList<personSearchModel> personSearchModelObservableList = FXCollections.observableArrayList();
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         DatabaseConnection dbConnect = new DatabaseConnection();
         Connection c = dbConnect.getConnection();
         //select query
@@ -53,16 +56,46 @@ public class personSearchController implements Initializable {
                  desc = queryOutPut.getString("description");
                 //populate observable list
                 personSearchModelObservableList.add(new personSearchModel(fname,lname,uname,pword,desc));
-
             }
             //propertyValueFactory corresponds to new personSearchModel fields
-            FN_tableColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
-            LN_tableColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
-            UN_tableColumn.setCellValueFactory(new PropertyValueFactory<>("userName"));
-            PW_tableColumn.setCellValueFactory(new PropertyValueFactory<>("passWord"));
-            DESC_tableColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+            FN_tableColumn.setCellValueFactory(new PropertyValueFactory<>("first_name"));
+            LN_tableColumn.setCellValueFactory(new PropertyValueFactory<>("last_name"));
+            UN_tableColumn.setCellValueFactory(new PropertyValueFactory<>("user_name"));
+            PW_tableColumn.setCellValueFactory(new PropertyValueFactory<>("pass_word"));
+            DESC_tableColumn.setCellValueFactory(new PropertyValueFactory<>("description_"));
 
             personSearchTableView.setItems(personSearchModelObservableList);
+
+            FilteredList<personSearchModel> filteredData = new FilteredList<>(personSearchModelObservableList,b->true);
+
+            searchPersonET.textProperty().addListener((observable,oldValue,newValue)->{
+                filteredData.setPredicate(personSearchModel -> {
+                    //if no search value, keep current records
+                    if(newValue.isEmpty()||newValue.isBlank()||newValue==null){
+                        return true;
+                    }
+                    String searchKeyword = newValue.toLowerCase();
+                    if(personSearchModel.getFirst_name().toLowerCase().indexOf(searchKeyword) > -1){
+                        return true; // match record found of first name
+                    }else if(personSearchModel.getLast_name().toLowerCase().indexOf(searchKeyword) > -1){
+                        return true; // match record found of last name
+                    }else if(personSearchModel.getUser_name().toLowerCase().indexOf(searchKeyword) > -1){
+                        return true;// match record found of user name
+                    }else if(personSearchModel.getPass_word().toLowerCase().indexOf(searchKeyword) > -1){
+                        return true;// match record found of password
+                    }else if(personSearchModel.getDescription_().toLowerCase().indexOf(searchKeyword) > -1){
+                        return true; // match record found of descriptions
+                    }else{
+                        return false;// no match found
+                    }
+                });
+            });
+            SortedList<personSearchModel> sortedData = new SortedList<>(filteredData);
+            //bind sorted result with table view
+            sortedData.comparatorProperty().bind(personSearchTableView.comparatorProperty());
+            //apply filtered and sorted data to table view
+            personSearchTableView.setItems(sortedData);
+
         }catch (SQLException e){
             Logger.getLogger(personSearchController.class.getName()).log(Level.SEVERE,null,e);
             e.printStackTrace();
